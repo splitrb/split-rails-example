cc6f0894629d10615bc9d4bfc806eeebcd2c16de
Jeffrey Chupp <jeff@semanticart.com>
2011-05-30 16:21:53 -0400

    initial skeleton app, created via "rails new" and then deleting some cruft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SplitRailsExample::Application.config.secret_token = 'c5f5859ee3fa861a57e4a0108135d447d14510cf7122258d7e085397edf838813592ce212672a1138e68a32e455d31cfe3adc2ffb9c409e62e5721aa827ddcb3'
